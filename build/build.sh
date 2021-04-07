ca5fe573dd3e3a9d9788a192840c158096f81b1d
Connor McFarlane <cm@semtex.net>
2021-04-08 00:15:17 +0100

    build script

--- build/build.sh
+++ build/build.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
+STEAMCMD_PATH="/opt/steam"
+APP_ID=730
+APP_PATH="./csgogame"
+
 mkdir out
 
-echo "building.."
+echo "## Making sure game is present and up-to-date.."
+
+$STEAMCMD_PATH/steamcmd.sh +login anonymous +force_install_dir $APP_PATH +app_update $APP_ID validate +quit
+
+echo "## Pretending to compile map.."
+
+ls $APP_PATH/csgo/bin
 
 sleep 5
+
+echo "## Complete!"
 
 echo "map" > out/mymap.bsp
 
